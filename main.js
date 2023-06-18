f7441bf8bb2e9c9052e80a0fcf6e1f093ef78237
hoang phi <moee1707@gmail.com>
2023-06-18 22:49:19 +0700

    modal

---
+++ main.js
@@ -0,0 +1,28 @@
+var modal = document.getElementById("myModal")
+var btn = document.getElementById("myBtn")
+var btn2 = document.getElementById("myBtn2")
+var span = document.getElementById("close")
+
+// btn.onclick = function() {
+//     modal.style.display = "block"
+// }
+
+// btn2.onclick = function() {
+//     modal.style.display = "block"
+// }
+
+span.onclick = function () {
+    modal.style.display = "none"
+}
+
+var sub = document.querySelectorAll(".subBtn")
+
+for (let i = 0; i < sub.length; i++) {
+  let currItem = sub[i]
+  currItem.addEventListener("click",() => {
+   modal.style.display = "block"
+  })
+    
+}
+
+
